a043d632d7e58ec5f617ced7b30b225112ee6a1b
Steammmmm <79504966+ajteter@users.noreply.github.com>
2025-08-25 17:05:35 +0800

    Update config.php

--- assets/includes/config.php
+++ assets/includes/config.php
@@ -1,22 +1,23 @@
 <?php
-$dbGM['host'] = 'localhost';
+$dbGM['host'] = 'ajteter-gm-ajteter-gm.i.aivencloud.com';
 # add your database name
-$dbGM['name'] = 'u319353809_xcmslite';
+$dbGM['name'] = 'defaultdb';
 
 
 # add your database user name
-$dbGM['user'] = 'ajteter';
+$dbGM['user'] = 'avnadmin';
 
 
 
 # add your database password
-$dbGM['pass'] = 'trustno1860812';
+$dbGM['pass'] = 'AVNS_jgcJdm8cOsFwKP5AjsL';
 
 
 
 
 #dont touch this
 $encryption = "vmbtrvw95105595885345**#3738s**A";
+
 
 
 #dont forget to add in deatabase gm_settings your domain website or else will not open other page except home page
